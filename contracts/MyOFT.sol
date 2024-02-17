f65bd70ba97b45959b73da7c0767c57dde5d4ce8
Andrii Ublinskykh <andreo.ublin24@gmail.com>
2024-02-17 17:07:51 +0200

    updated token for tests and ethers typechain

--- contracts/MyOFT.sol
+++ contracts/MyOFT.sol
@@ -4,12 +4,18 @@ pragma solidity 0.8.20;
 import "@layerzerolabs/solidity-examples/contracts/token/oft/v2/OFTV2.sol";
 
 contract MyOFT is OFTV2 {
-    constructor(
-        string memory _name,
-        string memory _symbol,
-        uint8 _sharedDecimals,
-        address _lzEndpoint
-    ) OFTV2 (_name, _symbol,_sharedDecimals,_lzEndpoint){
-        _mint(_msgSender(), 1_000_000_000 * 10**_sharedDecimals);
-    }
+  constructor(
+    string memory _name,
+    string memory _symbol,
+    uint8 _sharedDecimals,
+    address _lzEndpoint
+  ) OFTV2(_name, _symbol, _sharedDecimals, _lzEndpoint) {
+    _mint(_msgSender(), 1 ether * 10 ** _sharedDecimals);
+  }
+
+  function mint(address recipient, uint256 amount) onlyOwner {
+    require(recipient != address(0), "Must not be zero address");
+    require(amount > 0, "Zero amount");
+    _mint(receiver, amount);
+  }
 }
