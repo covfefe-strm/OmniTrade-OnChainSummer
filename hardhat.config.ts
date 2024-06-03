59028498fdbfd557275c1e72614d48b813f1bd13
Ublinskykh Andrii <andreo.ublin24@gmail.com>
2024-06-03 22:51:15 +0300

    added data for deploy on Celo, Moonbeam, Kava, Mantle, Linea, Scroll, Fraxtal chains

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,7 +1,8 @@
 import * as dotenv from "dotenv";
 import { HardhatUserConfig } from "hardhat/config";
+import "hardhat-contract-sizer";
 import "@nomicfoundation/hardhat-toolbox";
-import "@nomicfoundation/hardhat-ethers";
+// import "@nomicfoundation/hardhat-ethers";
 import "@nomicfoundation/hardhat-verify";
 import "solidity-coverage";
 // the sequence of dotenv.config() is very important
@@ -21,7 +22,11 @@ const {
   ARBISCAN_KEY,
   FANTOMSCAN_KEY,
   OPSCAN_KEY,
+  MOONBEAMSCAN_KEY,
   INFURA_API_KEY,
+  LINEASCAN_KEY,
+  SCROLLSCAN_KEY,
+  FRAXTALSCAN_KEY,
   QUICKNODE_KEY,
   COINMARKETCAP_KEY,
 } = process.env;
@@ -75,6 +80,43 @@ const config: HardhatUserConfig = {
       chainId: 44787,
       accounts: [`${DEV_PRIVATE_KEY}`],
     },
+    baseSepolia: {
+      url: "https://public.stackup.sh/api/v1/node/base-sepolia",
+      chainId: 84532,
+      accounts: [`${DEV_PRIVATE_KEY}`],
+    },
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${INFURA_API_KEY}`,
+      chainId: 1,
+      accounts: [`${PROD_PRIVATE_KEY}`],
+      gasPrice: 35000000000, //35 gwei
+    },
+    base: {
+      url: `https://base.meowrpc.com`,
+      chainId: 8453,
+      accounts: [`${PROD_PRIVATE_KEY}`],
+    },
+    polygon: {
+      url: `https://polygon-mainnet.infura.io/v3/${INFURA_API_KEY}`,
+      chainId: 137,
+      accounts: [`${PROD_PRIVATE_KEY}`],
+    },
+    bsc: {
+      url: `https://1rpc.io/bnb`,
+      chainId: 56,
+      gasPrice: 10000000000,
+      accounts: [`${PROD_PRIVATE_KEY}`],
+    },
+    optimisticEthereum: {
+      url: `https://optimism-mainnet.infura.io/v3/${INFURA_API_KEY}`,
+      chainId: 10,
+      accounts: [`${PROD_PRIVATE_KEY}`],
+    },
+    fantom: {
+      url: `https://rpcapi.fantom.network`,
+      chainId: 250,
+      accounts: [`${PROD_PRIVATE_KEY}`],
+    },
     avalanche: {
       url: `https://avalanche-mainnet.infura.io/v3/${INFURA_API_KEY}`,
       chainId: 43114,
@@ -88,44 +130,37 @@ const config: HardhatUserConfig = {
     celo: {
       url: `https://celo-mainnet.infura.io/v3/${INFURA_API_KEY}`,
       chainId: 42220,
-      accounts: [`${DEV_PRIVATE_KEY}`],
-    },
-    polygon: {
-      url: `https://polygon-mainnet.infura.io/v3/${INFURA_API_KEY}`,
-      chainId: 137,
       accounts: [`${PROD_PRIVATE_KEY}`],
     },
-    bsc: {
-      url: `https://1rpc.io/bnb`,
-      chainId: 56,
-      gasPrice: 10000000000,
+    moonbeam: {
+      url: `https://1rpc.io/glmr`,
+      chainId: 1284,
       accounts: [`${PROD_PRIVATE_KEY}`],
     },
-    mainnet: {
-      url: `https://mainnet.infura.io/v3/${INFURA_API_KEY}`,
-      chainId: 1,
-      accounts: [`${PROD_PRIVATE_KEY}`],
-      gasPrice: 35000000000, //35 gwei
-    },
-    optimisticEthereum: {
-      url: `https://optimism-mainnet.infura.io/v3/${INFURA_API_KEY}`,
-      chainId: 10,
+    kava: {
+      url: `https://evm.kava.io`,
+      chainId: 2222,
       accounts: [`${PROD_PRIVATE_KEY}`],
     },
-    base: {
-      url: `https://base.meowrpc.com`,
-      chainId: 8453,
+    mantle: {
+      url: `https://rpc.mantle.xyz`,
+      chainId: 5000,
       accounts: [`${PROD_PRIVATE_KEY}`],
     },
-    fantom: {
-      url: `https://rpcapi.fantom.network`,
-      chainId: 250,
+    linea: {
+      url: `https://1rpc.io/linea`,
+      chainId: 59144,
       accounts: [`${PROD_PRIVATE_KEY}`],
     },
-    baseSepolia: {
-      url: "https://public.stackup.sh/api/v1/node/base-sepolia",
-      chainId: 84532,
-      accounts: [`${DEV_PRIVATE_KEY}`],
+    scroll: {
+      url: `https://scroll.drpc.org`,
+      chainId: 534352,
+      accounts: [`${PROD_PRIVATE_KEY}`],
+    },
+    fraxtal: {
+      url: `https://rpc.frax.com`,
+      chainId: 252,
+      accounts: [`${PROD_PRIVATE_KEY}`],
     },
   },
   etherscan: {
@@ -143,9 +178,12 @@ const config: HardhatUserConfig = {
       baseSepolia: BASESCAN_KEY,
       optimisticEthereum: OPSCAN_KEY,
       fantom: FANTOMSCAN_KEY,
+      moonbeam: MOONBEAMSCAN_KEY,
+      kava: "asdasdasd", //api key is not required by the Kava explorer, but can't be empty
+      linea: LINEASCAN_KEY,
+      scroll: SCROLLSCAN_KEY,
+      fraxtal: FRAXTALSCAN_KEY,
     },
-    // apiKey: BSCSCAN_KEY,
-    // apiKey: MUMBAISCAN_KEY,
     customChains: [
       {
         network: "arbitrum",
@@ -193,6 +231,46 @@ const config: HardhatUserConfig = {
         urls: {
           apiURL: "https://api.ftmscan.com/api",
           browserURL: "https://ftmscan.com/",
+        },
+      },
+      {
+        network: "moonbeam",
+        chainId: 1284,
+        urls: {
+          apiURL: "https://api-moonbeam.moonscan.io/api",
+          browserURL: "https://moonscan.io/",
+        },
+      },
+      {
+        network: "kava",
+        chainId: 2222,
+        urls: {
+          apiURL: "https://kavascan.com/api",
+          browserURL: "https://kavascan.com",
+        },
+      },
+      {
+        network: "linea",
+        chainId: 59144,
+        urls: {
+          apiURL: "https://api.lineascan.build/api",
+          browserURL: "https://lineascan.build/",
+        },
+      },
+      {
+        network: "scroll",
+        chainId: 534352,
+        urls: {
+          apiURL: "https://api.scrollscan.com/api",
+          browserURL: "https://scrollscan.com/",
+        },
+      },
+      {
+        network: "fraxtal",
+        chainId: 252,
+        urls: {
+          apiURL: "https://api.fraxscan.com/api",
+          browserURL: "https://fraxscan.com/",
         },
       },
     ],
